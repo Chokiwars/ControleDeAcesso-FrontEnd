c2845f26c8267195c63e70c45083b4003bde1341
gabriel <galhiardogabriel@gmail.com>
2025-06-05 17:07:01 -0300

    feat: adicionando fetch para carregar os alunos

--- TelaListagem/script.js
+++ TelaListagem/script.js
@@ -1,7 +1,41 @@
-const toggleButton = document.getElementById('theme-toggle');
-const body = document.body;
+setInterval(() => {
+    let dataAtual = new Date();
+    document.getElementById("dataAtual").innerHTML = dataAtual.getDate() + "/" + (dataAtual.getMonth() + 1) + "/" + dataAtual.getFullYear() + " " + dataAtual.getHours() + ":" + dataAtual.getMinutes() + ":" + dataAtual.getSeconds();
+}, 1000);
 
-toggleButton.addEventListener('click', () => {
-  body.classList.toggle('dark-theme');  // Altera o tema do body
-  toggleButton.classList.toggle('dark'); // Altera a classe do botão
-});
+async function carregarAlunos() {
+  const url = "https://localhost:8080/alunos";
+  
+  try {
+    const response = await fetch(url, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const alunos = await response.json();
+    console.log("Alunos carregados:", alunos);
+    array.forEach(alunos => {
+      const alunoItem = document.createElement("tr");
+      alunoItem.innerHTML = `
+        <td>${alunos.id}</td>
+        <td>${alunos.nome}</td>
+        <td>${alunos.curso}</td>
+      `;
+
+      
+      document.getElementById("tabela").appendChild(alunoItem);
+      
+    });
+
+        
+  } catch (error) {
+    console.error("Erro ao carregar alunos:", error);
+    alert("Erro ao carregar alunos. Tente novamente mais tarde.");
+  }
+  
+}
